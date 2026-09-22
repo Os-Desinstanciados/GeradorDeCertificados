9d6846546112408e261ddad91e79faa119e29f62
vinicius.poli <alemaumvp@hotmail.com>
2026-09-22 16:54:02 -0300

    Adiciona CetificadoConfiguration

---
+++ src/Infraestrutura/Compartilhado/Orm/Config/CertificadoConfiguration.cs
@@ -0,0 +1,34 @@
+using GeradorDeCertificados.Dominio.Modulos.Certificado;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeradorDeCertificados.Infraestrutura.Compartilhado.Orm.Config;
+
+public sealed class CertificadoConfiguration : IEntityTypeConfiguration<Certificado>
+{
+    public void Configure(EntityTypeBuilder<Certificado> builder)
+    {
+        builder.ToTable("TBCertificados");
+
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).ValueGeneratedNever();
+
+        builder.Property(c => c.GeradorId)
+            .IsRequired();
+
+        builder.Property(c => c.NomeAluno)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(c => c.CaminhoArquivo)
+            .HasMaxLength(500)
+            .IsRequired(false);
+
+        builder.Property(c => c.DataGeracao)
+            .IsRequired(false);
+
+        builder.Property(c => c.Status)
+            .HasColumnName("Status")
+            .IsRequired();
+    }
+}
